2c35c68c074559916439b3fc1c9e6e1db80b4652
Ganapathi96 <ganapathi96001@gmail.com>
2022-12-27 11:24:35 +0530

    initialcommit

---
+++ src/javaprogram/simpleInterestCalculation.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class simpleInterestCalculation {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	float rateOfInterest=7.8f;
+	double amount=34000;
+	int year=5;
+	double interestAmount=(amount*year*rateOfInterest)/100;
+	System.out.println("intrest amonunt="+ interestAmount);
+	}
+
+}
